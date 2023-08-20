86576b0ab288c5b8fd6cbca776c00249054453ad
Viktor-Drykin <142505425+Viktor-Drykin@users.noreply.github.com>
2023-08-20 11:41:07 +0100

    init project

---
+++ FindFlightApp/FindFlightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindFlightAppApp.swift
+//  FindFlightApp
+//
+//  Created by Viktor Drykin on 20.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct FindFlightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
